178e2c4aea2aa53e9638f710f04aecf8daea664d
KaworuSSSS <143292307+KaworuSSSS@users.noreply.github.com>
2026-08-12 21:41:38 -0600

    Expose configuration objects to the window object

--- js/machine-config.js
+++ js/machine-config.js
@@ -1,14 +1,10 @@
-const WORK_AREA = {
-
+window.WORK_AREA = {
     X: 1100,
     Y: 700,
     Z: 700
-
 };
 
-
-const LIMITS = {
-
+window.LIMITS = {
     X_MIN: 0,
     X_MAX: WORK_AREA.X,
 
@@ -17,33 +13,19 @@ const LIMITS = {
 
     Z_MIN: 0,
     Z_MAX: WORK_AREA.Z
-
 };
 
-
-const MACHINE = {
-
+window.MACHINE = {
     tableX: WORK_AREA.X,
-
     tableY: WORK_AREA.Y,
-
     tableThickness: 40,
-
     frameHeight: 850,
-
     columnSize: 70,
-
     gantryThickness: 70,
-
     gantryDepth: 100,
-
     spindleWidth: 90,
-
     spindleDepth: 90,
-
     toolLength: 120
-
 };
 
-
-const TABLE_Y = 80;
+window.TABLE_Y = 80;
