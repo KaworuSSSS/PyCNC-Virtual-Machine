9d366e94cdc9eb33d59a88e86b43b9b3f836efe5
KaworuSSSS <143292307+KaworuSSSS@users.noreply.github.com>
2026-08-12 21:37:02 -0600

    Add machine configuration constants

---
+++ js/machine-config.js
@@ -0,0 +1,49 @@
+const WORK_AREA = {
+
+    X: 1100,
+    Y: 700,
+    Z: 700
+
+};
+
+
+const LIMITS = {
+
+    X_MIN: 0,
+    X_MAX: WORK_AREA.X,
+
+    Y_MIN: 0,
+    Y_MAX: WORK_AREA.Y,
+
+    Z_MIN: 0,
+    Z_MAX: WORK_AREA.Z
+
+};
+
+
+const MACHINE = {
+
+    tableX: WORK_AREA.X,
+
+    tableY: WORK_AREA.Y,
+
+    tableThickness: 40,
+
+    frameHeight: 850,
+
+    columnSize: 70,
+
+    gantryThickness: 70,
+
+    gantryDepth: 100,
+
+    spindleWidth: 90,
+
+    spindleDepth: 90,
+
+    toolLength: 120
+
+};
+
+
+const TABLE_Y = 80;
